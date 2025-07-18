a33472b4f7af9320b3b2347324e305bdd7575265
xyc <2039850123@qq.com>
2025-07-18 12:06:45 +0800

    客户端代码

---
+++ client/tftp_client.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <error.h>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string>
+class TFTPClient
+{
+private:
+    static const int PORT = 69;
+    static const int BUFFER_SIZE = 516;
+
+    int sfd;
+    struct sockaddr_in server_addr;
+
+    int doDownload();
+    int doUpload();
+    void waitForInput();
+    void clearScreen(); 
+    void showMenu();
+
+public:
+    TFTPClient(const std::string &serverIp);
+    ~TFTPClient();
+
+    void run();
+};
